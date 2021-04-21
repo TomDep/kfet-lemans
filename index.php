f93c1886755da0e139d516a3baab604f0470da7a
Tom de Pasquale <tomarthus72@gmail.com>
2021-04-21 11:41:40 +0200

    Initial Commit

---
+++ index.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Application Kfet - Accueil</title>
+</head>
+<body>
+
+	<ul>
+		<li>
+			<a href="pages/administration">Administrer</a>
+
+			<ul>
+				<li><a href="pages/administration#produits">Produits</a></li>
+				<li><a href="pages/administration#formules">Formules</a></li>
+				<li><a href="pages/administration#statut">Statut des usagers</a></li>
+				<li><a href="pages/administration#statistiques">Statistiques</a></li>
+				<li><a href="pages/administration#informations">Informations</a></li>
+			</ul>
+		</li>
+
+	</ul>
+
+</body>
+</html>

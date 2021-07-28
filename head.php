e3bac6f4ecd2d00cd2b86b14f51ebe9418375c8c
aksel-vaillant <aksel.vaillant@outlook.fr>
2021-07-28 11:42:28 +0200

    Adding menu and header

--- head.php
+++ head.php
@@ -1,4 +1,24 @@
-<meta charset="utf-8" />
-<meta lang="fr" />
+<meta charset="utf-8"/>
+<meta lang="fr"/>
 
+<!--Description's project-->
+<meta name="description" content="Site web de la KFet (ENSIM)">
+<meta name="author" content="Tom De Pasquale, Aksel Vaillant, Clément Le Bâtard">
+
+<!--Contains the menu, buttons, etc-->
 <link rel="stylesheet" type="text/css" href="css/global.css" />
+
+<!--Librairies-->
+
+	<!--Fonts from google-->
+	<link href="https://fonts.googleapis.com/css2?family=Catamaran:wght@300&family=Raleway:wght@400&family=Padauk&family=Dancing+Script:wght@700&display=swap" rel="stylesheet">
+
+	<!--Icons for menu-->
+	<script src="https://kit.fontawesome.com/37366ba499.js"></script>
+
+	<!--jQuery's library-->
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+
+
+
+
--- index.php
+++ index.php
@@ -15,54 +15,41 @@
 	<!-- Test coucou -->
 	<title>Kfet - Accueil</title>
 </head>
+
 <body>
 
-	<h1>Bonjour <?php echo htmlspecialchars($_SESSION['username']); ?></h1>
+	<?php include "templates/nav.php";?>
 
-	<nav class="menu">
-		<ul>
-			<li><a href="profile.php">Mon Compte</a></li>
-			<li>
-				<a href="administration.php">Administrer</a>
+    <section>
+    	<h1>Bonjour <?php echo htmlspecialchars($_SESSION['username']); ?></h1>
 
-				<ul>
-					<li><a href="administration.php#produits">Produits</a></li>
-					<li><a href="administration.php#formules">Formules</a></li>
-					<li><a href="administration.php#usagers">Usagers</a></li>
-					<li><a href="administration.php#statistiques">Statistiques</a></li>
-				</ul>
-			</li>
-			<li><a href="logout.php">Se déconnecter</a></li>
-		</ul>
-
-	<div class="add-user">
-		<h1>Ajouter un utilisateur</h1>
-		<form action="register.php" method="post" autocomplete="off">
-		
-			<label for="student_number">Numéro étudiant</label>
-			<input type="text" name="student_number" placeholder="182355" id="student_number" required>
-
-			<label for="username">Nom</label>
-			<input type="text" name="username" placeholder="Tom de Pasquale" id="username" required>
+		<div class="add-user">
+			<h1>Ajouter un utilisateur</h1>
+			<form action="register.php" method="post" autocomplete="off">
 			
-			<label for="bdlc_member">Abonné BDLC</label>
-			<input type="checkbox" name="bdlc_member" id="bdlc_member">
+				<label for="student_number">Numéro étudiant</label>
+				<input type="text" name="student_number" placeholder="182355" id="student_number" required>
 
-			<label for="auth_level">Niveau d'autorisation</label>
-			<select id="auth_level" name="auth_level" required>
-				<option value="0">Ensimien</option>
-				<option value="1">Barista</option>
+				<label for="username">Nom</label>
+				<input type="text" name="username" placeholder="Tom de Pasquale" id="username" required>
+				
+				<label for="bdlc_member">Abonné BDLC</label>
+				<input type="checkbox" name="bdlc_member" id="bdlc_member">
 
-<?php
-	// Add the administrator only if the administrator is logged on
-	if(isset($_SESSION['auth_level']) && $_SESSION['auth_level'] > 1)
-		echo '<option value="2">Administrateur</option>';
-?>
-			</select>
+				<label for="auth_level">Niveau d'autorisation</label>
+				<select id="auth_level" name="auth_level" required>
+					<option value="0">Ensimien</option>
+					<option value="1">Barista</option>
+					<?php
+						// Add the administrator only if the administrator is logged on
+						if(isset($_SESSION['auth_level']) && $_SESSION['auth_level'] > 1)
+							echo '<option value="2">Administrateur</option>';
+					?>
+				</select>
 
-			<input type="submit" value="Ajouter">
-		</form>
-	</div>
-
+				<input type="submit" value="Ajouter">
+			</form>
+		</div>
+	</section>
 </body>
 </html>

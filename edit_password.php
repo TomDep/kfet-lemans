ca73945c7528f5ff5fbd9fe0bac43b902d75c0ab
Tom de Pasquale <tomdepasquale@gmail.com>
2021-10-03 17:14:01 +0200

    Adding feedback messages

--- edit_password.php
+++ edit_password.php
@@ -10,55 +10,56 @@
 	// Redirect if the user isn't connected
 	if(!isset($_SESSION['logged_in']) || $_SESSION['logged_in'] != TRUE) {
 		header('Location: login.php');
-	}
+	} else {
 
-	// Process data from the form
-	$invalidPassword = FALSE;
-	$error = FALSE;
-	$emptyFields = FALSE;
-	if((empty($_POST['new_password']) || empty($_POST['password'])) && isset($_POST['submit'])) {
-		$emptyFields = TRUE;
-		$error = TRUE;
-	}
-
-	if(!isset($_POST['submit'], $_POST['password'], $_POST['new_password'])) {
-		$error = TRUE;
-	}
-
-	if(!$error) {
-		// Connect to the database
-		$connection = connectToDatabase();
-		if($connection == FALSE) {
-			exit();
+		// Process data from the form
+		$invalidPassword = FALSE;
+		$error = FALSE;
+		$emptyFields = FALSE;
+		if((empty($_POST['new_password']) || empty($_POST['password'])) && isset($_POST['submit'])) {
+			$emptyFields = TRUE;
+			$error = TRUE;
 		}
 
-		// Check if the given password matches the one saved
-		$req = 'SELECT password FROM users WHERE id = ?';
-		if ($stmt = $connection->prepare($req)) {
+		if(!isset($_POST['submit'], $_POST['password'], $_POST['new_password'])) {
+			$error = TRUE;
+		}
 
-			$stmt->bind_param('i', $_SESSION['id']);
-			$stmt->execute();
-			$stmt->store_result();
+		if(!$error) {
+			// Connect to the database
+			$connection = connectToDatabase();
+			if($connection == FALSE) {
+				header('Location: ?modify_password_status=database_error');
+			}
 
-			if ($stmt->num_rows > 0) {
-				$stmt->bind_result($password);
-				$stmt->fetch();
-				// We verify the password.
-				if (password_verify($_POST['password'], $password)) {
-					// Verification success! Now we update the password
-					$password_hashed = password_hash($_POST['new_password'], PASSWORD_DEFAULT);
-					
-					$req = 'UPDATE users SET password = ? WHERE id = ?';
-					if($stmt = $connection->prepare($req)) {
-						$stmt->bind_param('si', $password_hashed, $_SESSION['id']);
-						$stmt->execute();
+			// Check if the given password matches the one saved
+			$req = 'SELECT password FROM users WHERE id = ?';
+			if ($stmt = $connection->prepare($req)) {
+
+				$stmt->bind_param('i', $_SESSION['id']);
+				$stmt->execute();
+				$stmt->store_result();
+
+				if ($stmt->num_rows > 0) {
+					$stmt->bind_result($password);
+					$stmt->fetch();
+					// We verify the password.
+					if (password_verify($_POST['password'], $password)) {
+						// Verification success! Now we update the password
+						$password_hashed = password_hash($_POST['new_password'], PASSWORD_DEFAULT);
 						
-						// Redirect to index.php
-						header('Location: index.php');
+						$req = 'UPDATE users SET password = ? WHERE id = ?';
+						if($stmt = $connection->prepare($req)) {
+							$stmt->bind_param('si', $password_hashed, $_SESSION['id']);
+							$stmt->execute();
+							
+							// Redirect to index.php
+							header('Location: ?modify_password_status=success');
+						}
+					} else {
+						// Incorrect password
+						$invalidPassword = TRUE;
 					}
-				} else {
-					// Incorrect password
-					$invalidPassword = TRUE;
 				}
 			}
 		}
@@ -97,14 +98,25 @@
 	<?php
 		// Display an error message if the entered password was wrong
 		if($invalidPassword == TRUE) {
-			echo '<p>Le mot de passe entré est invalide</p>';
+			//echo '<p>Le mot de passe entré est invalide</p>';
+			header('Location: ?modify_password_status=invalid_password');
 		}
 
 		// In case the fields are empty
 		if($emptyFields == TRUE) {
-			echo '<p>Vous ne pouvez pas laisser les champs vide</p>';
+			//echo '<p>Vous ne pouvez pas laisser les champs vide</p>';
+			header('Location: ?modify_password_status=empty_field');
 		}
 	?>
 
-	</body>
+	<script src="js/status_message.js"></script>
+	<script type="text/javascript">
+		addStatusMessage('modify_password', {
+			'success' : 'Votre mot de passe a été modifié avec succès !',
+			'invalid_password' : 'Le mot de passe entré est invalide.',
+			'empty_field' : 'Vous ne pouvez pas entrer de mot de passe vide !',
+			'database_error' : 'Erreur lors de la connexion à la base de données'
+		})
+	</script>
+</body>
 </html>

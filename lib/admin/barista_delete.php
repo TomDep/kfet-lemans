bbc69b726edfead7ef9cbd4e5663c68188bf5b9e
Tom de Pasquale <tomdepasquale@gmail.com>
2021-10-24 18:27:17 +0200

    Delete the barista image and modify the authorization level

--- lib/admin/barista_delete.php
+++ lib/admin/barista_delete.php
@@ -12,6 +12,21 @@
     $mysqli = connectToDatabase();
 
     if(isset($_GET['id']) && !empty($_GET['id'])) {
+        // Delete the photo from the files
+        $req = 'SELECT photo FROM baristas WHERE id = ' .htmlspecialchars($_GET['id']);
+        if($result = $mysqli->query($req)) {
+            $photoName = $result->fetch_assoc()['photo'];
+            $result->close();
+
+            unlink('../../res/images/baristas/' . $photoName);
+        }
+
+        // Change the authorization level to 0
+        $req = 'UPDATE users SET auth_level = 0 WHERE id = (SELECT user_id FROM baristas WHERE id = '. htmlspecialchars($_GET['id']) .')';
+        if(!$mysqli->query($req)) {
+            echo $mysqli->error;
+        }
+
         // Delete from the database
         if($mysqli->query('DELETE FROM baristas WHERE id=' . htmlspecialchars($_GET['id']))) {
             header('Location: ../../administrate_baristas.php?delete_status=success');
